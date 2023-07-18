a0377d332f3fb08cbd3a5ee1ee29fa4bdaaa0077
cherry <771184240@qq.com>
2023-07-18 13:18:36 +0800

    0

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,7 @@ const CompressionWebpackPlugin = require('compression-webpack-plugin');
 
 
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '/init-vue-project/' : '/',
   productionSourceMap: false,
   transpileDependencies: true,
   lintOnSave: false,
