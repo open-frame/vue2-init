090a8b074a5e0feef252991ac2e602e9b6c3c641
易拉宝 <43592730+elarbor@users.noreply.github.com>
2024-11-20 17:53:33 +0800

    Delete src directory

--- src/pages/login/api.js
+++
@@ -1,7 +0,0 @@
-import {axios} from '@/service/index'
-
-
-// 账号登陆
-export function login(params, config) {
-  return axios.get("/service/demo/login.json", params, config)
-}
--- src/utils/authorization.js
+++
@@ -1,3 +0,0 @@
-if (Notification.permission !== "granted") {
-  Notification.requestPermission();
-}
